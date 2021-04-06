5534e072b6a1c9299d61deae2db8ff09969a08ec
Josef Hedblom <hedblomjosef@gmail.com>
2021-04-06 17:15:35 +0200

    Update ArrayUtils

--- ArrayUtils/ArrayUtils.js
+++ ArrayUtils/ArrayUtils.js
@@ -1,22 +1,17 @@
-// Multiply array elements with input value. 
+// Multiply elements with input value. 
 // [1,2,3,4] = [ 15, 30, 45, 60 ]
 Array.prototype.times = function (num) {
     return this.map(x => x * num);
-}
-
-// Capitalize the first letter of each word.
-// ["steve jobs"] = ["Steve Jobs"]
-Array.prototype.titleize = function() {
-    return this.map(letter => letter.titleize()).join(" ");
 };
 
 // Sums up the total value of all elements
-// [1,2,3,4] = 10
+// let array = [1,2,3,4] = 10
+// array.sum() = 10
 Array.prototype.sum = function(){
     return this.reduce(function(a, b){return a+b;})
 };
 
-// Finds the input value and removes it
+// Finds the input value of an array and removes it
 Array.prototype.removeItem = function(item){
 	let index = this.indexOf(item);
 	if(index > -1){
@@ -26,12 +21,12 @@ Array.prototype.removeItem = function(item){
 	
 };
 
-// Returns the last element
+// Returns the last element of the array
 Array.prototype.last = function(){
 	return this[this.length - 1]
 };
 
-// Return the smallest value
+// Return the smallet value
 Array.prototype.min = function(){
 	this.sort(function(a, b){return a - b});
 	return this[0]
