bd1269849484cda0d9d6125304ca3b7ac19d8aa2
Josef Hedblom <hedblomjosef@gmail.com>
2021-04-06 16:51:50 +0200

    Add method randomBigNumber and randomSmallNumber

--- MathUtils/MathUtils.js
+++ MathUtils/MathUtils.js
@@ -1,5 +1,9 @@
-Math.randomNumber = function (a) {
-    return Math.floor(Math.random() * a);
+Math.randomBigNumber = function () {
+    return Math.floor(Math.pow(10, 20-1) + Math.random() * 10 * Math.pow(10, 20-1));
+};
+
+Math.randomSmallNumber = function () {
+    return Math.floor(Math.random() * 100);
 };
 
 Math.isEven = function(a){
